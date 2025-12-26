f9b2c2dd1933edc9385c65999a286543a629efb5
Aditya_Prabhakar <professor130405@gmail.com>
2025-12-26 20:23:46 +0530

    Placeholder pages added

---
+++ pages/Sponsors.jsx
@@ -0,0 +1,7 @@
+export default function Sponsors() {
+    return(
+        <div id="Events" className="w-full h-[100vh] text-[#FFF9F0] font-[Creato_Display] text-[10.85vw] font-black leading-normal p-0 flex justify-center items-center">
+            Coming Soon...
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from "../pages/Home";
 import Navbar from "./assets/Components/Navbar";
 import Footer from "./assets/Components/Footer";
 import Schedule from "../pages/Schedule" 
+import Events from "../pages/Events";
+import OurTeam from "../pages/OurTeam";
+import Sponsors from "../pages/Sponsors";
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/schedule" element={<Schedule />} />
-        {/* <Route path="/events" element={<Events />} />
+        <Route path="/events" element={<Events />} />
         <Route path="/sponsors" element={<Sponsors />} />
-        <Route path="/team" element={<OurTeam />} /> */}
+        <Route path="/team" element={<OurTeam />} />
         
       </Routes>
       <Footer />
